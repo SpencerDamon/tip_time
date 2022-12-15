26feccf915f25ccad01f9adc3ca945dcbc9a3280
SpencerDamon <spencerdamon1974@gmail.com>
2022-12-15 13:24:19 -0800

    build test to calculate correct tip result

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.example.tiptime
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.Matchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,8 +24,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+//class  ExampleInstrumentedTest {
+class CalculatorTests {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
+    fun calculate_20_percent_tip() {
+        /*enter text into edit text view*/
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard()) //close soft keyboard so doesn't hide button
+        /*click calculate button*/
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        /*verify tip_result view asserts correct amount*/
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("10.00"))))
+    }
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
