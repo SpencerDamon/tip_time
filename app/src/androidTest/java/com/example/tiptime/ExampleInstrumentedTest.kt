2405adb776d85d71c40cc0571fb7aafd40e460d0
SpencerDamon <spencerdamon1974@gmail.com>
2022-12-15 13:52:13 -0800

    Add comments to test instrumentation

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -23,24 +23,27 @@ import org.junit.Rule
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+/*instrumentation test runner to run tests */
 @RunWith(AndroidJUnit4::class)
 //class  ExampleInstrumentedTest {
 class CalculatorTests {
+    /* specify subsequent rule */
     @get:Rule()
+    /* launch activity before every test*/
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun calculate_20_percent_tip() {
         /*enter text into edit text view*/
-        onView(withId(R.id.cost_of_service_edit_text))
-            .perform(typeText("50.00"))
+        onView(withId(R.id.cost_of_service_edit_text)) // onView() returns a ViewInteraction
+            .perform(typeText("50.00")) // typeText() returns a ViewAction
             .perform(ViewActions.closeSoftKeyboard()) //close soft keyboard so doesn't hide button
         /*click calculate button*/
-        onView(withId(R.id.calculate_button))
+        onView(withId(R.id.calculate_button)) // withId() returns a ViewMatcher
             .perform(click())
         /*verify tip_result view asserts correct amount*/
         onView(withId(R.id.tip_result))
-            .check(matches(withText(containsString("10.00"))))
+            .check(matches(withText(containsString("10.00")))) //check() gets passed a ViewAssertion
     }
     fun useAppContext() {
         // Context of the app under test.
